fbc9fb097f7f472294ce1827eb7c82b97d963010
Sravanksk <30795376+Sravanksk@users.noreply.github.com>
2020-01-13 14:08:52 +0530

    Update utilities.py

--- credentials_generator/utilities.py
+++ credentials_generator/utilities.py
@@ -2,16 +2,16 @@ import base64
 import random
 
 
-def stringToBase64(s):
+def string_to_base64(s):
     return base64.b64encode(bytes(s, 'utf-8')).decode()
 
 
 # Returns a securely generated random string. Source from the django.utils.crypto module.
-def getRandomString(length, allowed_chars='abcdefghijklmnopqrstuvwxyz' 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'):
+def get_random_string(length, allowed_chars='abcdefghijklmnopqrstuvwxyz' 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'):
     return ''.join(random.choice(allowed_chars) for i in range(length))
 
 
 # Create a random secret key. Source from the django.utils.crypto module.
-def getSecretKey():
+def get_secret_key():
     chars = 'abcdefghijklmnopqrstuvwxyz0123456789'
-    return getRandomString(40, chars)
+    return get_random_string(40, chars)
